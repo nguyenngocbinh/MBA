08fb4bc9cf2b6cc83115a513fb747abf251ca598
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2019-10-17 08:49:40 +0700

    create

---
+++ R/mba.R
@@ -0,0 +1,12 @@
+#' \code{market.basket.analysis} package
+#'
+#' Google spreadsheets R API
+#'
+#'
+#' @docType package
+#' @name market.basket.analysis
+#' @import knitr
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
